9cd3b98ed42c082b0811abaa3fb095fe0be8758f
clefersondannylo <clefersonnascimento8@gmail.com>
2022-04-24 15:55:49 -0300

    criando a navegação entre componentes

--- src/App.js
+++ src/App.js
@@ -21,17 +21,31 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
-  console.log(words);
+
+  //Start the secret word game
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  //Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  //Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
   return (
     <div className="App">
       {
-        gameStage === 'start' && <StartScreen />
+        gameStage === 'start' && <StartScreen startGame={startGame}/>
       }
       {
-        gameStage === 'game' && <Game />
+        gameStage === 'game' && <Game verifyLetter={verifyLetter} />
       }
       {
-        gameStage === 'end' && <GameOver />
+        gameStage === 'end' && <GameOver retry={retry} />
       }
     </div>
   );
